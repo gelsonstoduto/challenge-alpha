9b59944f73f274b0ab8550a6c6551c541240acca
Gelson Lopes Stoduto <gelson.stoduto@foton.la>
2024-03-23 18:44:52 -0300

    :recycle: criação do sampleData.

---
+++ StarWars/app/src/main/java/br/com/gstoduto/starwars/util/Constants.kt
@@ -0,0 +1,6 @@
+package br.com.gstoduto.starwars.util
+
+object Constants {
+    // Urls bases para pegar as imagens
+    const val BASE_URL_MOVIE_IMAGE = "https://starwars-visualguide.com/assets/img/films/"
+}
