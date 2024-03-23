4fbeb92ab87699e40ac9634786dbbda48c3f2319
Gelson Lopes Stoduto <gelson.stoduto@foton.la>
2024-03-23 18:24:00 -0300

    :recycle: Inclusão da classe dao de movie.

---
+++ StarWars/app/src/main/java/br/com/gstoduto/starwars/database/dao/MovieDao.kt
@@ -0,0 +1,37 @@
+package br.com.gstoduto.starwars.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import br.com.gstoduto.starwars.database.entities.MovieEntity
+import br.com.gstoduto.starwars.model.Movie
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MovieDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun save(movie: MovieEntity)
+
+    @Query("SELECT * FROM movies")
+    fun findAll(): Flow<List<Movie>>
+
+    @Query("SELECT * FROM movies WHERE inMyList = 1")
+    fun myList(): Flow<List<MovieEntity>>
+
+    @Query("UPDATE movies SET inMyList = 0 WHERE title = :title")
+    suspend fun removeFromMyList(title: String)
+
+    @Query("UPDATE movies SET inMyList = 1 WHERE title = :title")
+    suspend fun addToMyList(title: String)
+
+    @Query("SELECT * FROM movies WHERE title = :title")
+    fun findMovieByTitle(title: String): Flow<MovieEntity>
+
+    @Query("SELECT * FROM movies WHERE inMyList = 0 and title != :title")
+    fun suggestedMovies(title: String): Flow<List<Movie>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveAll(vararg entities: MovieEntity)
+}
