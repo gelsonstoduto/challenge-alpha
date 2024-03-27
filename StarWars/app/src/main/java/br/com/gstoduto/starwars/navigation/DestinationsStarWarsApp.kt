9d75d8a9eb45eadd2e3178447a6910820f396e28
Gelson Lopes Stoduto <gelson.stoduto@foton.la>
2024-03-27 14:45:58 -0300

    :recycle: inclusão da rota de detahes do filme.

--- StarWars/app/src/main/java/br/com/gstoduto/starwars/navigation/DestinationsStarWarsApp.kt
+++ StarWars/app/src/main/java/br/com/gstoduto/starwars/navigation/DestinationsStarWarsApp.kt
@@ -1,6 +1,21 @@
 package br.com.gstoduto.starwars.navigation
 
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import br.com.gstoduto.starwars.util.Constants.ID_MOVIE
+
 sealed class DestinationsStarWarsApp(val rota: String) {
     object homeRoute : DestinationsStarWarsApp("home")
     object myListRoute : DestinationsStarWarsApp("myList")
+
+    object MovieDetails {
+        const val rota = "movieDetails"
+        const val routeWithArguments = "$rota/{$ID_MOVIE}"
+        val arguments = listOf(
+            navArgument(ID_MOVIE) {
+                defaultValue = ""
+                type = NavType.StringType
+            }
+        )
+    }
 }
--- StarWars/app/src/main/java/br/com/gstoduto/starwars/util/Constants.kt
+++ StarWars/app/src/main/java/br/com/gstoduto/starwars/util/Constants.kt
@@ -4,4 +4,7 @@ object Constants {
     // Urls bases para pegar as imagens
     const val BASE_URL = "https://swapi.dev/api/"
     const val BASE_URL_MOVIE_IMAGE = "https://starwars-visualguide.com/assets/img/films/"
+
+    // constante para setar movieId
+    const val ID_MOVIE = "id_movie"
 }
