1c78edfb85e58c6badf67a19ff805e825ab9b9de
Gelson Lopes Stoduto <gelson.stoduto@foton.la>
2024-03-27 14:41:14 -0300

    :recycle: ajuste na sampleData.

--- StarWars/app/src/main/java/br/com/gstoduto/starwars/sampleData/SampleData.kt
+++ StarWars/app/src/main/java/br/com/gstoduto/starwars/sampleData/SampleData.kt
@@ -16,16 +16,16 @@ val randomTitle
     get() = LoremIpsum(Random.nextInt(1, 5)).values
         .first().toString()
 val randomDescription
-    get() = LoremIpsum(Random.nextInt(5, 30)).values
+    get() = LoremIpsum(Random.nextInt(1, 5)).values
         .first().toString()
 val randomDirector
-    get() = LoremIpsum(Random.nextInt(30, 45)).values
+    get() = LoremIpsum(Random.nextInt(1, 5)).values
         .first().toString()
 val randomProducer
-    get() = LoremIpsum(Random.nextInt(45, 60)).values
+    get() = LoremIpsum(Random.nextInt(1, 5)).values
         .first().toString()
 val randomEpisode
-    get() = LoremIpsum(Random.nextInt(60, 75)).values
+    get() = LoremIpsum(Random.nextInt(1, 5)).values
         .first().toString()
 
 val sampleMovies = List(15) {
@@ -36,6 +36,28 @@ val sampleMovies = List(15) {
         episodeId = randomEpisode,
         director = randomDirector,
         producer = randomProducer,
-        inMyList = it % 2 == 0
+        inMyList = true
     )
 }
+
+val sampleMovieAdded =
+    Movie(
+        title = randomTitle,
+        url = randomImage,
+        openingCrawl = randomDescription,
+        episodeId = randomEpisode,
+        director = randomDirector,
+        producer = randomProducer,
+        inMyList = false
+    )
+
+val sampleMovieRemoved =
+    Movie(
+        title = randomTitle,
+        url = randomImage,
+        openingCrawl = randomDescription,
+        episodeId = randomEpisode,
+        director = randomDirector,
+        producer = randomProducer,
+        inMyList = true
+    )
