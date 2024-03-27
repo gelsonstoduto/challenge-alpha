7122cfa870d523eac002619625b4f83ce9ffe7af
Gelson Lopes Stoduto <gelson.stoduto@foton.la>
2024-03-27 19:04:11 -0300

    :coffin: Inclusão do Dispatchers.IO na busca de filmes.

--- StarWars/app/src/main/java/br/com/gstoduto/starwars/repositories/MovieRepository.kt
+++ StarWars/app/src/main/java/br/com/gstoduto/starwars/repositories/MovieRepository.kt
@@ -7,18 +7,18 @@ import br.com.gstoduto.starwars.model.Movie
 import br.com.gstoduto.starwars.network.model.toMovieEntity
 import br.com.gstoduto.starwars.network.services.MovieService
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import java.net.ConnectException
 import javax.inject.Inject
-import kotlin.coroutines.coroutineContext
 
 class MovieRepository @Inject constructor(
     private val dao: MovieDao,
     private val service: MovieService
 ) {
     suspend fun findMovies(): Flow<List<Movie>> {
-        CoroutineScope(coroutineContext).launch {
+        CoroutineScope(Dispatchers.IO).launch {
             try {
                 val response = service.findAll()
                 val entities = response.results.map { it.toMovieEntity() }
