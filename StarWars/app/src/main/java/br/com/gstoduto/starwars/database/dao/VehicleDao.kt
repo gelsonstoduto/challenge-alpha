57c5d40cec2ee27ca0313a0871deaa236c619021
Gelson Lopes Stoduto <gelson.stoduto@foton.la>
2024-03-27 21:28:23 -0300

    :recycle: criação da VehicleDao.

---
+++ StarWars/app/src/main/java/br/com/gstoduto/starwars/database/dao/VehicleDao.kt
@@ -0,0 +1,34 @@
+package br.com.gstoduto.starwars.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import br.com.gstoduto.starwars.database.entities.VehicleEntity
+import br.com.gstoduto.starwars.model.Vehicle
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface VehicleDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun save(vehicle: VehicleEntity)
+
+    @Query("SELECT * FROM vehicles")
+    fun findAll(): Flow<List<Vehicle>>
+
+    @Query("SELECT * FROM vehicles WHERE inMyList = 1")
+    fun myList(): Flow<List<VehicleEntity>>
+
+    @Query("UPDATE vehicles SET inMyList = 0 WHERE name = :id")
+    suspend fun removeFromMyList(id: String)
+
+    @Query("UPDATE vehicles SET inMyList = 1 WHERE name = :id")
+    suspend fun addToMyList(id: String)
+
+    @Query("SELECT * FROM vehicles WHERE name = :id")
+    fun findVehicleById(id: String?): Flow<VehicleEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveAll(vararg entities: VehicleEntity)
+}
