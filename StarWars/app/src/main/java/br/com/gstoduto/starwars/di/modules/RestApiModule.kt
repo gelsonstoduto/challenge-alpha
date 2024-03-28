f62ebb2b02893df1abf956d0e99f6a97228f5c9e
Gelson Lopes Stoduto <gelson.stoduto@foton.la>
2024-03-27 21:36:07 -0300

    :recycle: Inclusao da provideVehicleService.

--- StarWars/app/src/main/java/br/com/gstoduto/starwars/di/modules/RestApiModule.kt
+++ StarWars/app/src/main/java/br/com/gstoduto/starwars/di/modules/RestApiModule.kt
@@ -1,6 +1,7 @@
 package br.com.gstoduto.starwars.di.modules
 
 import br.com.gstoduto.starwars.network.services.MovieService
+import br.com.gstoduto.starwars.network.services.VehicleService
 import br.com.gstoduto.starwars.util.Constants.BASE_URL
 import dagger.Module
 import dagger.Provides
@@ -30,6 +31,12 @@ object RestApiModule {
     @Singleton
     fun provideMovieService(retrofit: Retrofit): MovieService {
         return retrofit.create(MovieService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideVehicleService(retrofit: Retrofit): VehicleService {
+        return retrofit.create(VehicleService::class.java)
     }
 
     @Provides
