fecaf75e6bcbcf13ab6acab84070c1a180453da9
Gelson Lopes Stoduto <gelson.stoduto@foton.la>
2024-03-27 21:33:16 -0300

    :recycle: Criacao da classe VehicleUiState.

---
+++ StarWars/app/src/main/java/br/com/gstoduto/starwars/ui/uistates/VehicleUiState.kt
@@ -0,0 +1,14 @@
+package br.com.gstoduto.starwars.ui.uistates
+
+import br.com.gstoduto.starwars.model.Vehicle
+
+sealed class VehicleUiState {
+    object Loading : VehicleUiState()
+
+    object Empty : VehicleUiState()
+
+    data class Success(
+        val vehicles: List<Vehicle> = emptyList()
+    ) : VehicleUiState()
+
+}
