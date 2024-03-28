9d62c558f3343db6bb77c00cdf24d2270f3f10dd
Gelson Lopes Stoduto <gelson.stoduto@foton.la>
2024-03-27 21:39:17 -0300

    :recycle: Alteração do nome do metodo.

--- StarWars/app/src/main/java/br/com/gstoduto/starwars/network/services/MovieService.kt
+++ StarWars/app/src/main/java/br/com/gstoduto/starwars/network/services/MovieService.kt
@@ -1,10 +1,10 @@
 package br.com.gstoduto.starwars.network.services
 
-import br.com.gstoduto.starwars.network.model.FilmsResponse
+import br.com.gstoduto.starwars.network.model.MoviesResponse
 import retrofit2.http.GET
 
 interface MovieService {
 
     @GET("films")
-    suspend fun findAll(): FilmsResponse
+    suspend fun findAll(): MoviesResponse
 }
