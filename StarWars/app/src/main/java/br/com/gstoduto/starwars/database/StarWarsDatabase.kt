bffe573aae413c937f39e8d990522f714bf89bf1
Gelson Lopes Stoduto <gelson.stoduto@foton.la>
2024-03-27 21:36:55 -0300

    :recycle: Atualizacao do database com vehicleDao.

--- StarWars/app/src/main/java/br/com/gstoduto/starwars/database/StarWarsDatabase.kt
+++ StarWars/app/src/main/java/br/com/gstoduto/starwars/database/StarWarsDatabase.kt
@@ -3,14 +3,17 @@ package br.com.gstoduto.starwars.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import br.com.gstoduto.starwars.database.dao.MovieDao
+import br.com.gstoduto.starwars.database.dao.VehicleDao
 import br.com.gstoduto.starwars.database.entities.MovieEntity
+import br.com.gstoduto.starwars.database.entities.VehicleEntity
 
 @Database(
-    version = 1,
-    entities = [MovieEntity::class],
+    version = 2,
+    entities = [MovieEntity::class, VehicleEntity::class],
 )
 abstract class StarWarsDatabase : RoomDatabase() {
 
     abstract fun movieDao(): MovieDao
+    abstract fun vehicleDao(): VehicleDao
 
 }
