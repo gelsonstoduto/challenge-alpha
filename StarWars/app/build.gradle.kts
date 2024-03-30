fe45b36ba3700806e82a869a08af10015838f06e
Gelson Lopes Stoduto <gelson.stoduto@foton.la>
2024-03-29 21:17:52 -0300

    :wrench: inclusão da dependencia do data store.

--- StarWars/app/build.gradle.kts
+++ StarWars/app/build.gradle.kts
@@ -66,6 +66,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.6.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.datastore:datastore-core:1.0.0")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
